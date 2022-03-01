9abd29f48e6f45fc4e71415e7531d5c402bec22a
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-28 20:35:46 -0800

    create auth variables.

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import {getFirestore} from "firebase/firestore";
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
 const firebaseConfig = {
   apiKey: "AIzaSyBcRlU1jL5hTUYzXWCt43GL2_ALUdwP7k8",
   authDomain: "action-planner-8e70c.firebaseapp.com",
@@ -15,3 +13,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();

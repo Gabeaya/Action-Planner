4f3901231e0eaa9cbe48f4db7a86edbb4ddf092e
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-28 18:49:52 -0800

    import register component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Register from './components/Register';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main className="App">
+      <Register />
+    </main>
   );
 }
 

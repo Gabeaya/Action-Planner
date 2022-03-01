2c9ea931bae75cdeed7691fdbf20da19fb310ae1
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-28 20:19:22 -0800

    Connect firebase.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcRlU1jL5hTUYzXWCt43GL2_ALUdwP7k8",
+  authDomain: "action-planner-8e70c.firebaseapp.com",
+  projectId: "action-planner-8e70c",
+  storageBucket: "action-planner-8e70c.appspot.com",
+  messagingSenderId: "680462843127",
+  appId: "1:680462843127:web:8df58e815d8bdbfd1127f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

4941482651ae4c1093c9a856313cc1d0eb0b3a0a
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-05 15:46:17 -0800

    Create pathways for navigating through components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import Authentication from './components/Authentication';
+
 function App() {
-  return (
-    <main className="App">
-    </main>
+  return (  
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/createpost" element={<CreatePost />}/>
+        <Route path="/authentication" element={<Authentication />}/>
+      </Routes>
+    </Router>
   );
 }
 

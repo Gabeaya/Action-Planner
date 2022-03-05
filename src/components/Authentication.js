247d7f55afd547db5dc5214860a7ce2cdb5591cb
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-05 15:58:45 -0800

    Pass the setIsAuth hook to login page and apply the popup from firebase auth/

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { auth } from '../firebase'
+import {auth, provider} from "../firebase-config";
+import {signInWithPopup} from 'firebase/auth';
 
-function Authentication() {
+function Authentication({setIsAuth}) {
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+
+    })
+  }
   return (
     <div className="loginPage">
       <p> Sign in with Google</p>

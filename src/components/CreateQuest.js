921d8955f7f3815bf4dd10c0b47a1994a3cae051
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-06 09:24:32 -0800

    Change name of the post page.

---
+++ src/components/CreateQuest.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function CreateQuest() {
+  return (
+    <div className='createPostPage'>
+      <div className='cpContainer'>
+        <h1> Begin a new quest </h1>
+        <div className='inputGp'> </div>
+        <div className="inputGp"></div>
+      </div>
+    </div>
+  )
+}
+
+export default CreateQuest

e9f19712d7c9c44e040871a2650c20f20b75ecc8
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-06 15:50:12 -0800

    Add quest deletion to home.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import {getDocs, collection} from 'firebase/firestore';
-import {db} from '../firebase';
-function Home() {
+import {getDocs, collection, deleteDoc, doc} from 'firebase/firestore';
+import {auth, db} from '../firebase';
+function Home({isAuth}) {
   const [questLists, setQuestLists] = useState([]);
-  const questsCollectionRef = collection(db, "quests")
+  const questsCollectionRef = collection(db, "quests");
+  
+  const deleteQuest = async (id) => {
+    const questDoc = doc(db, "quests", id)
+    await deleteDoc(questDoc)
+
+  };
   useEffect(()=> {
     const getQuests = async () => {
       const data = await getDocs(questsCollectionRef)
@@ -12,14 +18,37 @@ function Home() {
 
     getQuests();
   },[])
+
+  
+
   return (
 
     <div className="homepage">
       {questLists.map((quest) => {
-      return <div className="quest"> {quest.questTitle}</div>
-    })}
+        return (
+          <div className="quest"> 
+            <div className="header"> 
+              <div className='title'> 
+                <h1>{quest.questTitle}</h1>
+              </div>
+              <div className='deletePost'>
+                {isAuth && quest.author.id === auth.currentUser.uid && (
+                  <button 
+                    onClick={() => {
+                      deleteQuest(quest.id);
+                    }}
+                  >
+                    X
+                  </button>
+                )}
+              </div>
+            </div>
+              <div className='questTextContainer'>{quest.questOrigin}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
-export default Home
+export default Home;

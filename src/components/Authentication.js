758bba1d5ad45e5bcd88b7a1ee875175101fda6a
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-05 16:10:13 -0800

    Add navigation and the google sign in function to the login button.

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { auth } from '../firebase'
-import {auth, provider} from "../firebase-config";
+import {auth, provider} from "../firebase";
 import {signInWithPopup} from 'firebase/auth';
-
+import {useNavigate} from 'react-router-dom';
 function Authentication({setIsAuth}) {
+  let navigate = useNavigate();
   const signInWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true)
+      navigate("/");
+    });
+  };
 
-    })
-  }
   return (
     <div className="loginPage">
       <p> Sign in with Google</p>
-      <button className='login-with-google-btn'>Sign In</button>
+      <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign In</button>
     </div>
   )
 }

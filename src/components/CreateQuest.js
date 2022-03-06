8e03583afccc22095bc7fda24bfec8f263316698
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-06 10:45:22 -0800

    create functions and hooks to manage dynamic mission inputs and add a form for creating new quests.

--- src/components/CreateQuest.js
+++ src/components/CreateQuest.js
@@ -1,14 +1,60 @@
 import React from 'react'
 
-function CreateQuest() {
+function CreateQuest({missionValues, setMissionValues}) {
+  const handleChange = (i, e) => {
+    const newMissionValues = [...missionValues];
+    newMissionValues[i][e.target.mission] = e.target.value;
+    setMissionValues(newMissionValues);
+  }
+
+const addFormFields = () => {
+  setMissionValues([...missionValues, {mission: ""}])
+}
+
+const removeFormFields = (i) => {
+  const newMissionValues = [...missionValues];
+  newMissionValues.splice(i, 1);
+  setMissionValues(newMissionValues)
+}
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  alert(JSON.stringify(missionValues));
+}
   return (
-    <div className='createPostPage'>
-      <div className='cpContainer'>
-        <h1> Begin a new quest </h1>
-        <div className='inputGp'> </div>
-        <div className="inputGp"></div>
+    <form onSubmit={handleSubmit}>
+      <div className='createPostPage'>
+        <div className='cpContainer'>
+          <h1> Cast a new quest </h1>
+          <div className='inputGp'> 
+            <label> Quest Name:</label>
+            <input placeholder='Title...'/>
+          </div>
+          <div className="inputGp">
+            <label> Quest Description: </label>
+            <textarea placeholder='Description...'/>
+          </div>
+          <div className='inputGp'>
+            {missionValues.map((element, index) => (
+              <div className='form-inline' key={index}>
+                <label>Mission</label>
+                <input type='text' name='form' value={element.mission || ""} onChange={e => handleChange(index, e)} />
+                {
+                  index ?
+                  <button type='button' className='button remove' onClick={() => removeFormFields(index)}>Remove</button>
+                  : null 
+                }
+              </div>
+            ))}
+            <div className="button-section">
+              <button className="button add" type="button" onClick={() => addFormFields()}>Add</button>
+              <button className="button submit" type="submit">Submit</button>
+          </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </form>
+    
   )
 }
 

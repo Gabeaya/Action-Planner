e13f5e3a42997ca507fd94bb9576c57f66a241be
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-06 12:36:27 -0800

    Add the onchange states for title origin and date.

--- src/components/CreateQuest.js
+++ src/components/CreateQuest.js
@@ -1,13 +1,18 @@
-import React from 'react'
-
+import React, { useState} from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
+import {addDoc} from "firebase/firestore"
 function CreateQuest({missionValues, setMissionValues}) {
+  const [questTitle, setQuestTitle] = useState("");
+  const [questOrigin, setQuestOrigin] = useState("");
+  const [selectedDate, setSelectedDate ] = useState(null)
+
   const handleChange = (e, index) => {
     const {name, value} = e.target
     const newMissionValues = [...missionValues];
     newMissionValues[index][name] = value;
     setMissionValues(newMissionValues)
   };
-
 
   const addFormFields = () => {
     setMissionValues([...missionValues, {mission: ""}])
@@ -25,24 +30,54 @@ function CreateQuest({missionValues, setMissionValues}) {
   }
 
   console.log(missionValues);
-  
+
+  const createQuest = async () =>
   return (
     <form onSubmit={handleSubmit}>
       <div className='createPostPage'>
         <div className='cpContainer'>
-          <h1> Cast a new quest </h1>
+          <h1> Begin a Quest </h1>
           <div className='inputGp'> 
             <label> Quest Name:</label>
-            <input placeholder='Title...'/>
+            <br/>
+            <input 
+              placeholder='Title...' 
+              onChange={(event) => {
+                setQuestTitle(event.target.value);
+              }}
+            />
           </div>
           <div className="inputGp">
-            <label> Quest Description: </label>
-            <textarea placeholder='Description...'/>
+            <label> Quest Origin: </label>
+            <br />
+            <textarea 
+              placeholder='Where does your quest begin...'
+              onChange={(event) => {
+                setQuestOrigin(event.target.value);
+              }}
+            />
           </div>
+          <div className='datePicker'>
+            <label>Quest Deadline</label>
+            
+              <DatePicker 
+                selected={selectedDate}
+                onChange={
+                  date => setSelectedDate(date)
+                }
+                minDate={new Date()}
+                isClearable
+                showYearDropdown
+                scrollableMonthYearDropdown
+              />
+            
+          </div>
+          
           <div className='inputGp'>
             {missionValues.map((element, index) => (
               <div className='form-inline' key={index}>
                 <label>Mission</label>
+                <br />
                 <input type='text' name='mission' value={element.mission} onChange={(e) => handleChange(e, index)} />
                 {
                   index ?
@@ -51,9 +86,12 @@ function CreateQuest({missionValues, setMissionValues}) {
                 }
               </div>
             ))}
+            <br/>
             <div className="button-section">
-              <button className="button add" type="button" onClick={() => addFormFields()}>Add</button>
-              <button className="button submit" type="submit">Submit</button>
+              <button className="button add" type="button" onClick={() => addFormFields()}>Add Mission</button>
+
+              <br/>
+              <button className="button submit" type="submit">Begin Quest!</button>
           </div>
           </div>
         </div>

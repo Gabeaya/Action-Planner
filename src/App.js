c64c91d0fca7490454c5f945bd9956b9a515b7b7
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-08 15:01:11 -0800

    Remove unneeded imports.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Home from './components/Home';
 import CreateQuest from './components/CreateQuest';
@@ -14,28 +14,9 @@ function App() {
 
   const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
 
-  // const signUserOut = () => {
-  //   signOut(auth).then(() => {
-  //     localStorage.clear()
-  //     setIsAuth(false)
-  //     window.location.pathname = "/authentication";
-  //   })
-  // };
-
   return (  
     <Router>
       <Navbar isAuth={isAuth} setIsAuth={setIsAuth}/>
-      {/* <nav>
-        <Link to="/"> Home</Link>
-        {!isAuth ? (
-          <Link to="/authentication"> Login </Link> 
-        ) : (
-          <>
-          <Link to="/createquest"> Begin a Quest</Link>
-          <button onClick={signUserOut}> Log Out</button>
-          </>
-        )}
-      </nav> */}
       <Routes>
         <Route path="/" element={<Home isAuth={isAuth} />}/>
         <Route path="/createquest"  element={<CreateQuest missionValues={missionValues} setMissionValues={setMissionValues}/>}/>

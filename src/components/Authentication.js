0c90d1d070ee04e46e839214986b98d48d737b97
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-08 14:02:12 -0800

    Add landing page info.

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -13,10 +13,17 @@ function Authentication({setIsAuth}) {
   };
 
   return (
-    <div className="loginPage">
-      <p> Sign in with Google</p>
-      <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign In</button>
-    </div>
+    <>
+      <div className='landingPage'>
+        <h1>Welcome to the Battle Within</h1>
+        <p>There is an old Native American tale explaining this: Within us there are two beasts, one that feeds on our good habits and the other feeds on the bad habits. The one you feed the most is the one that triumphs the other. This app is intended to track/manifest better habits while also break down goals into smaller steps.</p>
+      </div>
+      <div className="loginPage">
+        <p> Sign in with Google</p>
+        <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign In</button>
+      </div>
+    </>
+    
   )
 }
 

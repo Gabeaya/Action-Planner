47a900ab2028180f7f2937190fe2c55dc9ee08f0
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-09 20:48:19 -0800

    Import css.

--- src/components/CreateQuest.js
+++ src/components/CreateQuest.js
@@ -2,6 +2,7 @@ import React, { useState} from 'react';
 import {addDoc, collection} from "firebase/firestore";
 import { db, auth } from '../firebase';
 import { useNavigate} from "react-router-dom";
+import './App.css';
 
 function CreateQuest({ missionValues, setMissionValues}) {
   const [questTitle, setQuestTitle] = useState("");
@@ -30,10 +31,13 @@ function CreateQuest({ missionValues, setMissionValues}) {
   const questsCollectionRef = collection(db, "quests")
   let navigate = useNavigate();
   
+
+
   const createQuest = async (e) => {
     e.preventDefault();
 
     await addDoc(questsCollectionRef, {
+      
       questTitle, 
       questOrigin, 
       selectedDate, 
@@ -45,65 +49,68 @@ function CreateQuest({ missionValues, setMissionValues}) {
 
   
   return (
-    <form>
-      <div className='createPostPage'>
-        <div className='cpContainer'>
-          <h1> Begin a Quest </h1>
-          <div className='inputGp'> 
-            <label> Quest Name:</label>
-            <br/>
-            <input 
-              placeholder='Title...' 
-              onChange={(event) => {
-                setQuestTitle(event.target.value);
-              }}
-            />
-          </div>
-          <div className="inputGp">
-            <label> Quest Origin: </label>
-            <br />
-            <textarea 
-              placeholder='Where does your quest begin...'
-              onChange={(event) => {
-                setQuestOrigin(event.target.value);
-              }}
-            />
-          </div>
-          <div className='datePicker'>
-            <label>Quest Deadline</label>
-            <input
-              type="date"
-              onChange={(event) => {
-                setSelectedDate(event.target.value)
-              }}
-            />
-            
-          </div>
-          
-          <div className='inputGp'>
-            {missionValues.map((element, index) => (
-              <div className='form-inline' key={index}>
-                <label>Mission</label>
-                <br />
-                <input type='text' name='mission' value={element.mission} onChange={(e) => handleChange(e, index)} />
-                {
-                  index ?
-                  <button type='button' className='button remove' onClick={() => removeFormFields(index)}>Remove</button>
-                  : null 
-                }
-              </div>
-            ))}
-            <br/>
-            <div className="button-section">
-              <button className="button add" type="button" onClick={() => addFormFields()}>Add Mission</button>
-
+    <main>
+      <form>
+        <div className='createPostPage'>
+          <div className='cpContainer'>
+            <h1> Begin a Quest </h1>
+            <div className='inputGp'> 
+              <label> Quest Name:</label>
               <br/>
-              <button onClick={createQuest} className="button submit" type="submit">Begin Quest!</button>
+              <input 
+                placeholder='Title...' 
+                onChange={(event) => {
+                  setQuestTitle(event.target.value);
+                }}
+              />
+            </div>
+            <div className="inputGp">
+              <label> Quest Origin: </label>
+              <br />
+              <textarea 
+                placeholder='Where does your quest begin...'
+                onChange={(event) => {
+                  setQuestOrigin(event.target.value);
+                }}
+              />
+            </div>
+            <div className='datePicker'>
+              <label>Quest Deadline</label>
+              <input
+                type="date"
+                onChange={(event) => {
+                  setSelectedDate(event.target.value)
+                }}
+              />
+              
+            </div>
+            
+            <div className='inputGp'>
+              {missionValues.map((element, index) => (
+                <div className='form-inline' key={index}>
+                  <label>Mission</label>
+                  <br />
+                  <input type='text' name='mission' value={element.mission} onChange={(e) => handleChange(e, index)} />
+                  {
+                    index ?
+                    <button type='button' className='button remove' onClick={() => removeFormFields(index)}>Remove</button>
+                    : null 
+                  }
+                </div>
+              ))}
+              <br/>
+              <div className="button-section">
+                <button className="button add" type="button" onClick={() => addFormFields()}>Add Mission</button>
+
+                <br/>
+                <button onClick={createQuest} className="button submit" type="submit">Begin Quest!</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </main>
+    
     
   )
 }

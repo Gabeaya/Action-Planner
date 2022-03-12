0343b3c52c62f06d5242e11c34e58826a7034524
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-11 21:20:47 -0800

    Add the same styling from the form page to your auth page.

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -13,7 +13,7 @@ function Authentication({setIsAuth}) {
   };
 
   return (
-    <>
+    <div className='container'>
       <div className='landingPage'>
         <h1>Welcome to the Battle Within</h1>
         <p>Within all of us there resides a battle between two wolves. The good one feeds on our joy, peace, love, hope, serenity, generosity, empathy, kindness, and truth. They envigorate the good wolf. Then there is the evil on that feasts on the bad habits. Our anger, envy, jealousy, sorrow, regret, greed, arrogance, self-pity, guilt, resentment, inferiority, lies, false pride, and ego empower him. The wolf that wins that battle is the one you feed. This app is intended to track/manifest better habits so that you may become good and powerful. Take you goals create attainable steps that coincide with deadlines so that you may put a timeline on your dreams.</p>
@@ -22,7 +22,8 @@ function Authentication({setIsAuth}) {
         <p> Sign in with Google to Begin</p>
         <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign In</button>
       </div>
-    </>
+    </div>
+
     
   )
 }

5f595aae9de0c1c31e781b8c5ee6aaca6fff518f
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-12 19:41:33 -0800

    Move the story to another link.

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -16,23 +16,30 @@ function Authentication({setIsAuth}) {
   return (
     <>
       <div className='landingPage'>
-      <div className='container2'>
-        <div className='front side'>
-          <div className="content"> 
-            <h1>WELCOME TO<br/>THE BATTLE WITHIN</h1>
-          </div>
-        </div>
-        <div className='back side'>
-          <div className='content'>
-            <p>Within all of us there resides a battle between two wolves. The good one feeds on our joy, peace, love, hope, serenity, generosity, empathy, kindness, and truth. They envigorate the good wolf. Then there is the evil on that feasts on the bad habits. Our anger, envy, jealousy, sorrow, regret, greed, arrogance, self-pity, guilt, resentment, inferiority, lies, false pride, and ego empower him. The wolf that wins that battle is the one you feed. This app is intended to track/manifest better habits so that you may become good and powerful. Take you goals create attainable steps that coincide with deadlines so that you may put a timeline on your dreams.</p>
+        <div className='container2'>
+          <div className='front side'>
+            <div className="content"> 
+              <h1>WELCOME TO<br/><br/>THE BATTLE WITHIN</h1>
             </div>
           </div>
-        {/* <div className="loginPage">
-          <p> Sign in with Google to Begin</p>
-          <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign In</button>
-        </div>  */}
+          <div className='back side'>
+            <div className='content'>
+              <h2>An Action Planning App</h2>
+              <ul>
+                <li>Choose your goals</li>
+                <br/>
+                <li>Determine a deadline</li>
+                <br/>
+                <li>Break down goals into feasible steps</li>
+                <br/>
+                <li>Complete your goals to gain XP</li>
+              </ul>
+              <p>Get Started by Signing with Google</p>
+              <button className='login-with-google-btn' onClick={signInWithGoogle}>Sign In</button>
+            </div>
+          </div> 
+        </div>
       </div>
-    </div>
     <Footer/>
     </>
     

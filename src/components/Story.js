afb8f56b919bbcd37fa3626133b539e73d07dc17
Gabriel Ayala <gabeayala100@gmail.com>
2022-03-12 19:42:58 -0800

    Add a story component.

---
+++ src/components/Story.js
@@ -0,0 +1,18 @@
+<p>Within all of us there resides 
+  a battle between two wolves. 
+  The good one feeds on our joy, peace, 
+  love, hope, serenity, generosity, 
+  empathy, kindness, and truth. They 
+  envigorate the good wolf. Then 
+  there is the evil on that feasts on 
+  the bad habits. Our anger, envy, 
+  jealousy, sorrow, regret, greed, 
+  arrogance, self-pity, guilt, resentment, 
+  inferiority, lies, false pride, and ego 
+  empower him. The wolf that wins that battle 
+  is the one you feed. This app is intended
+  to track/manifest better habits so that 
+  you may become good and powerful. Take 
+  you goals create attainable steps that 
+  coincide with deadlines so that you may 
+  put a timeline on your dreams.</p>
